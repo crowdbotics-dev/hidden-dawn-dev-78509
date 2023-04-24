e2a33d79a07c57524dc0f76e1508eb0b7bc63789
Crowdbotics <engineering@crowdbotics.com>
2023-04-24 00:07:37 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "hidden-dawn-dev-78509",
+  url: "https://hidden-dawn-dev-78509.botics.co",
+  api: "https://hidden-dawn-dev-78509.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
